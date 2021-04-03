adf92e87b9af7e6def548eb2d52125433dc0900c
Sumana Sattar <sumana.sattar@hyperisland.se>
2021-04-03 15:31:34 +0200

    animations are added to menu items

--- src/components/menu-items/menu-item.component.jsx
+++ src/components/menu-items/menu-item.component.jsx
@@ -2,9 +2,10 @@ import './menu-item.styles.scss'
 const MenuItem = ({title , image , size}) => {
     return (
         
-                <div className={`${size} menu-item`} style={{backgroundImage:`url(${image})`}}>
+                <div className={`${size} menu-item`}>
+                    <div className='backGround' style={{backgroundImage:`url(${image})`}}/>
                     <div className='content'>
-                        <h1 className='title'>{title}</h1>
+                        <h1 className='title'>{title.toUpperCase()}</h1>
                         <span className='subtitle'>shop now</span>
                     </div>
                 </div>
