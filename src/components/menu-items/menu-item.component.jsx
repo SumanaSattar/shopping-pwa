a0da377ff4e7c8e7ec339f0381032f8dc2d7afa7
Sumana Sattar <sumana.sattar@hyperisland.se>
2021-04-03 14:31:53 +0200

    images and title are added as props

--- src/components/menu-items/menu-item.component.jsx
+++ src/components/menu-items/menu-item.component.jsx
@@ -1,8 +1,8 @@
 import './menu-item.styles.scss'
-const MenuItem = ({title}) => {
+const MenuItem = ({title , image , size}) => {
     return (
         
-                <div className='menu-item'>
+                <div className={`${size} menu-item`} style={{backgroundImage:`url(${image})`}}>
                     <div className='content'>
                         <h1 className='title'>{title}</h1>
                         <span className='subtitle'>shop now</span>
--- src/pages/homepage/HomePageComponenet.jsx
+++ src/pages/homepage/HomePageComponenet.jsx
@@ -1,11 +1,9 @@
 import './homepage.styles.scss'
-import MenuItem from'./../../components/menu-items/menu-item.component';
+import Directory from './../../components/directory/directory.component'
 const HomePage = () => {
     return (
-        <div>
-          <MenuItem title="Hat"/>
-          <MenuItem title="Cloths"/>
-          <MenuItem title="Pants"/>
+        <div className='homepage'>
+            <Directory/>
         </div>
        
       );
