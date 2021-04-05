614988c0e11f563c73de9fa0ba6c5fd9577bd2b1
Sumana Sattar <50830293+SumanaSattar@users.noreply.github.com>
2021-04-05 15:46:36 +0200

    shop folder added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomePage from './pages/homepage/HomePageComponenet'
+import ShopPage from './pages/shop/shop.component'
 import {Route , Switch} from 'react-router-dom'
  
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <Switch>
       <Route exact path='/' component={HomePage}></Route>
+      <Route  path='/shops' component={ShopPage}></Route>
     </Switch>
       
   );
