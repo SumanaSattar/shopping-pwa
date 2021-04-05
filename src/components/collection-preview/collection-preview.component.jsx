7ef4b80f733e542df155bae7e26367b3c0939fb6
Sumana Sattar <50830293+SumanaSattar@users.noreply.github.com>
2021-04-05 16:36:26 +0200

    shop page working styles added

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,13 +1,13 @@
 import './collection-preview.styles.scss'
-
+import CollectionItems from './../collection-items/collection-items.component'
 
 const CollectionPreview = ({title,items}) => {
     return ( 
         <div className='collection-preview'>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <div className='preview'>
-                 {items.filter((item,index)=> index < 4).map((item)=>(
-                    <div key={item.key}>{item.name}</div>
+                 {items.filter((item,index)=> index < 4).map(({id , ...otherCollectionProps})=>(
+                    <CollectionItems key={id}{...otherCollectionProps}/>
                 ))}
             </div>
         </div>
