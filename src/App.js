dae09be429a245f045d89cb700144b254387bc36
Sumana Sattar <50830293+SumanaSattar@users.noreply.github.com>
2021-04-06 21:22:48 +0200

    header added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import HomePage from './pages/homepage/HomePageComponenet'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 import {Route , Switch} from 'react-router-dom'
+
  
 
 import './App.css';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={HomePage}></Route>
-      <Route  path='/shops' component={ShopPage}></Route>
-    </Switch>
+    <div>
+      <Header/>
+      <Switch>
+         <Route exact path='/' component={HomePage}></Route>
+         <Route  path='/shop' component={ShopPage}></Route>
+      </Switch>
+    </div>
+    
       
   );
 }
